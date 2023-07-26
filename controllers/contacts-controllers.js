f3ed5e1a45c3495af4fb781f4662c78d9c52d97e
RomanOdessa75 <romeowrc@ukr.net>
2023-07-26 20:46:22 +0300

    node-hw03-mongodb-separated-controllers

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -19,3 +19,71 @@ const getAll = async (req, res, next) => {
     next(error);
   }
 };
+
+const getById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      throw HttpError(404, `Not found`);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const add = async (req, res, next) => {
+  try {
+    const { error } = contactValidator(req.body);
+    if (error) {
+      throw HttpError(400, "missing required name field");
+    }
+    const contact = await addContact(req.body);
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await removeContact(contactId);
+    if (!contact) {
+      throw HttpError(404, `Not found`);
+    }
+    res.status(200).json({ message: "contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateById = async (req, res, next) => {
+  try {
+    const { error } = contactValidator(req.body);
+    if (error) {
+      throw HttpError(400, "missing fields");
+    }
+    const { name, email, phone } = req.body;
+    const { contactId } = req.params;
+    if (!name && !email && !phone) {
+      res.status(400).json({ message: "missing fields" });
+    }
+    const contact = await updateContact(contactId, req.body);
+    if (!contact) {
+      throw HttpError(404, `Contact with id=${contactId} is not found`);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+  add,
+  deleteById,
+  updateById,
+};
